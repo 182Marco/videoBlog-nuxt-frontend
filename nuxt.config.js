90c341453ccc617b118ba6ffc9eba7e6235e4da1
182Marco <marcomilza@gmail.com>
2023-01-15 19:56:04 +0100

    updated nuxt config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,6 +74,13 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 
+  image: {
+    domains: [
+    'localhost:8000', 
+    'api-mirkofasolifilmmaker.visualfarm.it',
+    'localhost']
+  },
+
   env: {
     baseUrl: process.env.BASE_URL,
     // || "http://localhost:3000",
